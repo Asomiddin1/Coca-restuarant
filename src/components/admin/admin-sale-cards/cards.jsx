34051d44d83a4c896ccb56b6cd53a5bc4b996905
beharus <sd979141@gmail.com.com>
2023-07-21 16:42:20 -0800

    asd

---
+++ src/components/admin/admin-sale-cards/cards.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Cards = () => {
+    let date = [
+        {
+            text: 'Total orders',
+            icon: 'fa-solid fa-file',
+            iconColor: 'text-neutral-500',
+            money: '342',
+            percent: "4.2%",
+            percentColor: 'text-lime-500',
+            trend: "fa-solid fa-arrow-up"
+        },
+        {
+            text: 'Total sales',
+            icon: 'fa-solid fa-person-rays',
+            iconColor: 'text-neutral-500',
+            money: '$1,290',
+            percent: "2.2%",
+            percentColor: 'text-lime-500',
+            trend: "fa-solid fa-arrow-up"
+        },
+        {
+            text: 'Net sales',
+            icon: 'fa-solid fa-filter-circle-dollar',
+            iconColor: 'text-neutral-500',
+            money: '$1,940',
+            percent: "4.0%",
+            percentColor: 'text-lime-500',
+            trend: "fa-solid fa-arrow-up"
+        },
+        {
+            text: 'Cancelled orders',
+            icon: 'fa-solid fa-circle-xmark',
+            iconColor: 'text-orange-600',
+            money: '12',
+            percent: "1.5%",
+            percentColor: 'text-orange-600',
+            trend: "fa-solid fa-arrow-down"
+        }
+    ]
+    return (
+        <div className=" justify-start w-full items-start flex-wrap xl:flex-nowrap gap-4 inline-flex">
+            {date.map(({ text, icon, money, percent, percentColor, trend, iconColor }, index) => {
+                return (
+                    <div key={index} className="min-w-[220px] xl:w-full grow shrink basis-0 rounded-xl drop-shadow justify-start items-start gap-4 bg-white flex">
+                        <div className="grow shrink basis-0 bg-white rounded-xl flex-col justify-start items-start inline-flex">
+                            <div className="w-full h-[164px] px-6 pt-6 pb-4 bg-white rounded-xl shadow flex-col justify-start items-start gap-6 flex">
+                                <div className="self-stretch justify-start items-center gap-3 inline-flex">
+                                    <div className={`w-6 h-6 relative ${iconColor}`}><i className={icon}></i></div>
+                                    <div className="grow shrink basis-0 text-zinc-900 text-base font-medium leading-normal">{text}</div>
+                                </div>
+                                <div className="self-stretch h-[72px] flex-col justify-center items-start gap-1 flex">
+                                    <div className="text-zinc-900 text-[40px] font-semibold leading-[48px]">{money}</div>
+                                    <div className="h-5 relative flex gap-2">
+                                        <div className={`${percentColor} flex items-center`}>
+                                            <div className={` text-sm font-semibold leading-tight`}><i className={trend}></i></div>
+                                            <div className="">
+                                                <div className="text-sm font-semibold leading-tight" >{percent}</div>
+                                            </div>
+                                        </div>
+                                        <div className=" text-zinc-500 text-xs font-medium leading-[18px]">from last month</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className=" w-full px-6 py-4 justify-between items-center gap-4 inline-flex">
+                                <div className="grow shrink basis-0 text-orange-600 text-sm font-semibold leading-tight">View detail</div>
+                                <i class="fa-solid fa-chevron-right text-orange-600"></i>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Cards
---
+++ src/components/admin/linechart/line.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, DataLabel, LineSeries, MultiColoredLineSeries } from '@syncfusion/ej2-react-charts'
+
+const Line = () => {
+
+    const data = [[
+        { x: ' ', y: 292, color: '#FECF24' },
+        { x: 'Mon, 20', y: 335, color: '#FECF24' },
+        { x: 'Tue, 21', y: 414, color: '#FECF24' },
+        { x: 'Wed, 22', y: 365, color: '#FECF24' },
+        { x: 'Thu, 23', y: 343, color: '#FECF24' },
+        { x: 'Fri, 24', y: 357, color: '#FECF24' },
+        { x: 'Sat, 25', y: 213, color: '#FECF24' },
+        { x: 'Sun, 26', y: 400, color: '#FECF24' },
+        { x: 'Mon, 27', y: 381, color: '#FECF24' },
+        { x: 'Tue, 28', y: 404, color: '#FECF24' },
+    ], [
+        { x: ' ', y: 274, color: '#B1E71D' },
+        { x: 'Mon, 20', y: 304, color: '#B1E71D' },
+        { x: 'Tue, 21', y: 401, color: '#B1E71D' },
+        { x: 'Wed, 22', y: 352, color: '#B1E71D' },
+        { x: 'Thu, 23', y: 436, color: '#B1E71D' },
+        { x: 'Fri, 24', y: 404, color: '#B1E71D' },
+        { x: 'Sat, 25', y: 446, color: '#B1E71D' },
+        { x: 'Sun, 26', y: 298, color: '#B1E71D' },
+        { x: 'Mon, 27', y: 293, color: '#B1E71D' },
+        { x: 'Tue, 28', y: 307, color: '#B1E71D' },
+    ], [
+        { x: ' ', y: 265, color: '#FF736A' },
+        { x: 'Mon, 20', y: 281, color: '#FF736A' },
+        { x: 'Tue, 21', y: 292, color: '#FF736A' },
+        { x: 'Wed, 22', y: 341, color: '#FF736A' },
+        { x: 'Thu, 23', y: 338, color: '#FF736A' },
+        { x: 'Fri, 24', y: 314, color: '#FF736A' },
+        { x: 'Sat, 25', y: 327, color: '#FF736A' },
+        { x: 'Sun, 26', y: 423, color: '#FF736A' },
+        { x: 'Mon, 27', y: 415, color: '#FF736A' },
+        { x: 'Tue, 28', y: 407, color: '#FF736A' },
+    ], [
+        { x: ' ', y: 241, color: '#73CB50' },
+        { x: 'Mon, 20', y: 274, color: '#73CB50' },
+        { x: 'Tue, 21', y: 284, color: '#73CB50' },
+        { x: 'Wed, 22', y: 271, color: '#73CB50' },
+        { x: 'Thu, 23', y: 316, color: '#73CB50' },
+        { x: 'Fri, 24', y: 294, color: '#73CB50' },
+        { x: 'Sat, 25', y: 296, color: '#73CB50' },
+        { x: 'Sun, 26', y: 378, color: '#73CB50' },
+        { x: 'Mon, 27', y: 438, color: '#73CB50' },
+        { x: 'Tue, 28', y: 407, color: '#73CB50' },
+    ], [
+        { x: ' ', y: 178, color: '#8569F6' },
+        { x: 'Mon, 20', y: 198, color: '#8569F6' },
+        { x: 'Tue, 21', y: 249, color: '#8569F6' },
+        { x: 'Wed, 22', y: 310, color: '#8569F6' },
+        { x: 'Thu, 23', y: 283, color: '#8569F6' },
+        { x: 'Fri, 24', y: 297, color: '#8569F6' },
+        { x: 'Sat, 25', y: 186, color: '#8569F6' },
+        { x: 'Sun, 26', y: 316, color: '#8569F6' },
+        { x: 'Mon, 27', y: 389, color: '#8569F6' },
+        { x: 'Tue, 28', y: 397, color: '#8569F6' },
+    ],]
+    const marker = { visible: true, width: 4, height: 4 };
+    return (
+        <div className="w-[630px]">
+            <ChartComponent
+                id='line-chart'
+                height="220px"
+                primaryXAxis={{
+                    valueType: 'Category'
+                }}
+                tooltip={{
+                    enable: true, format: '<b>${point.x} /  ${point.y}</b>'
+                }}
+                primaryYAxis={{
+                    minimum: 100, maximum: 500, interval: 200,
+                    lineStyle: { width: 0 },
+                    majorTickLines: { width: 0 },
+                    minorTickLines: { width: 0 },
+
+                    rangePadding: 'None',
+                }}
+
+                legendSettings={{ background: 'white' }}
+                chartArea={{ border: { width: 0 } }}
+            // markerSettings={{ visible: ['All'], size: 2.5, fill: 'red' }}
+            ><Inject services={[LineSeries, Legend, Tooltip, DataLabel, Category, MultiColoredLineSeries]} />
+                <SeriesCollectionDirective>
+                    <SeriesDirective type='MultiColoredLine' pointColorMapping='color' marker={marker} dataSource={data[0]} xName='x' yName='y' width={2}>
+                    </SeriesDirective>
+                    <SeriesDirective type='MultiColoredLine' pointColorMapping='color' marker={marker} dataSource={data[1]} xName='x' yName='y' width={2}>
+                    </SeriesDirective>
+                    <SeriesDirective type='MultiColoredLine' pointColorMapping='color' marker={marker} dataSource={data[2]} xName='x' yName='y' width={2}>
+                    </SeriesDirective>
+                    <SeriesDirective type='MultiColoredLine' pointColorMapping='color' marker={marker} dataSource={data[3]} xName='x' yName='y' width={2}>
+                    </SeriesDirective>
+                    <SeriesDirective type='MultiColoredLine' pointColorMapping='color' marker={marker} dataSource={data[4]} xName='x' yName='y' width={2}>
+                    </SeriesDirective>
+                </SeriesCollectionDirective>
+            </ChartComponent>
+        </div>
+    )
+}
+
+export default Line
